8cd168df604afb0e16391ee01c8c2f6b4b25eca1
ambsenyestudi <adrifre7@hotmail.com>
2022-11-05 19:36:00 +0100

    Board has needed cells red

--- test/BoardShould.cs
+++ test/BoardShould.cs
@@ -10,5 +10,17 @@ namespace ConwaysGameOfLife.Test
         {
             Assert.Throws<CellAmountException>(() => new Board(2, new List<Cell>()));
         }
+
+        [Fact]
+        public void HaveNeededCells()
+        {
+            var board = new Board(2, new List<Cell> { 
+                new Cell(false),
+                new Cell(true), 
+                new Cell(false),
+                new Cell(true)
+            });
+            Assert.Equal(4, board.Cells.Count);
+        }
     }
 }
