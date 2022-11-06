81f089f09aa97b1d0c85e171e601d4ba8234c3ff
ambsenyestudi <adrifre7@hotmail.com>
2022-11-06 13:14:41 +0100

    Refactor move method

--- src/Board.cs
+++ src/Board.cs
@@ -21,41 +21,15 @@ namespace ConwaysGameOfLife.Domain
 
         public List<Cell> GetNeighours(int index)
         {
-            List<int> indexes = GetNeighboursIndexes(index);
-            List<Cell> neighours = Cells
-                .Where(x => indexes.Contains(x.Id))
+            List<int> indexes = NeighbourService.GetNeighboursIndexes(Bounds, index)
+                .OrderBy(x => x)
                 .ToList();
-            return neighours;
-        }
 
-        private List<int> GetNeighboursIndexes(int index) =>
-             GetNeighbours(index).OrderBy(x => x).ToList();
-
-        private List<int> GetNeighbours(int index)
-        {
-            var (x, y) = NeighbourService.To2DCoordinates(Bounds, index);
-            if (index == Bounds.GetTopLeft())
-            {
-                return new List<int> { NeighbourService.ToRight(Bounds,x, y), NeighbourService.ToBottomRight(Bounds,x, y), NeighbourService.ToBottom(Bounds,x, y) };
-            }
-            if (index == Bounds.GetTopRight())
-            {
-                return new List<int> { NeighbourService.ToLeft(Bounds,x, y), NeighbourService.ToLeftBottom(Bounds,x, y), NeighbourService.ToBottom(Bounds,x, y) };
-            }
-            if (index == Bounds.GetBottomLeft())
-            {
-                return new List<int> { NeighbourService.ToTop(Bounds, x, y), NeighbourService.ToTopRight(Bounds,x, y), NeighbourService.ToRight(Bounds,x, y) };
-            }
-            if (index == Bounds.GetBottomRight())
-            {
-                return new List<int> { NeighbourService.ToTopLeft(Bounds,x, y), NeighbourService.ToTop(Bounds, x, y), NeighbourService.ToLeft(Bounds,x, y) };
-            }
-            return new List<int> { NeighbourService.ToLeft(Bounds,x, y), NeighbourService.ToTopLeft(Bounds,x, y), 
-                NeighbourService.ToTop(Bounds, x, y), NeighbourService.ToTopRight(Bounds,x, y), NeighbourService.ToRight(Bounds,x, y), 
-                NeighbourService.ToBottomRight(Bounds,x, y), NeighbourService.ToBottom(Bounds,x, y), NeighbourService.ToLeftBottom(Bounds,x, y)
-            };
+            return Cells
+                .Where(x => indexes.Contains(x.Id))
+                .ToList(); ;
         }
-                
+        
         private static int GetExpectedCellCount(int size)=>
             (int) Math.Pow(size, 2);
     }
--- src/Cells/NeighbourService.cs
+++ src/Cells/NeighbourService.cs
@@ -2,6 +2,31 @@
 {
     public class NeighbourService
     {
+        public static List<int> GetNeighboursIndexes(Bounds bounds, int index)
+        {
+            var (x, y) = To2DCoordinates(bounds, index);
+            if (index == bounds.GetTopLeft())
+            {
+                return new List<int> { ToRight(bounds, x, y), ToBottomRight(bounds, x, y), ToBottom(bounds, x, y) };
+            }
+            if (index == bounds.GetTopRight())
+            {
+                return new List<int> { ToLeft(bounds, x, y), ToLeftBottom(bounds, x, y), ToBottom(bounds, x, y) };
+            }
+            if (index == bounds.GetBottomLeft())
+            {
+                return new List<int> { ToTop(bounds, x, y), ToTopRight(bounds, x, y), ToRight(bounds, x, y) };
+            }
+            if (index == bounds.GetBottomRight())
+            {
+                return new List<int> { ToTopLeft(bounds, x, y), ToTop(bounds, x, y), ToLeft(bounds, x, y) };
+            }
+            return new List<int> { ToLeft(bounds,x, y), ToTopLeft(bounds,x, y),
+                ToTop(bounds, x, y), ToTopRight(bounds,x, y), ToRight(bounds,x, y),
+                ToBottomRight(bounds,x, y), ToBottom(bounds,x, y), ToLeftBottom(bounds,x, y)
+            };
+        }
+
         public static int ToLeftBottom(Bounds bounds, int x, int y) =>
             (y + 1) * bounds.Width + (x - 1);
         public static int ToBottom(Bounds bounds, int x, int y) =>
