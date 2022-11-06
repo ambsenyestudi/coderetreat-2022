00fa747d7bc78f22fe8c0a4c7554934f4f5f9eb5
ambsenyestudi <adrifre7@hotmail.com>
2022-11-06 11:57:09 +0100

    Extract named methods

--- src/Board.cs
+++ src/Board.cs
@@ -33,15 +33,36 @@ namespace ConwaysGameOfLife.Domain
             var (x, y) = To2DCoordinates(index);
 
             var indexes = new List<int>
-            {
-                y * Size + (x-1), (y-1) * Size + (x-1), (y-1) * Size + x,
-                (y - 1) * Size + (x+1), y * Size + (x+1), (y+1) * Size + (x+1),
-                (y+1) * Size + x, (y+1) * Size + (x-1)
+            { ToLeft(x, y), ToLeftTop(x, y), ToTop(x, y),
+                ToTopRight(x, y), ToRight(x, y), ToRightBottom(x, y),
+                ToBottom(x, y), ToLeftBottom(x, y)
             }.Where(x => x >= 0)
             .OrderBy(x => x)
             .ToList();
             return indexes;
         }
+
+        private int ToLeftBottom(int x, int y) =>
+            (y + 1) * Size + (x - 1);
+
+        private int ToBottom(int x, int y) =>
+            (y + 1) * Size + x;
+
+        private int ToRightBottom(int x, int y) => 
+            (y + 1) * Size + (x + 1);
+
+        private int ToRight(int x, int y) =>
+            y * Size + (x + 1);
+
+        private int ToTopRight(int x, int y) =>
+            (y - 1) * Size + (x + 1);
+
+        private int ToLeft(int x, int y) =>
+            y * Size + (x - 1);
+        private int ToLeftTop(int x, int y) =>
+            (y - 1) * Size + (x - 1);
+        private int ToTop(int x, int y) =>
+            (y - 1) * Size + x;
 
         private (int, int) To2DCoordinates(int index) =>
             (index % Size, index / Size);
